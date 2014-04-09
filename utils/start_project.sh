29e26aa20e5b5c8645d819c02636298e96458a89
Sergey Cheparev <s.cheparev@s-v.de>
2014-04-09 17:58:56 +0200

    Added start_project script

---
+++ utils/start_project.sh
@@ -0,0 +1,15 @@
+git clone --depth 1 --no-hardlinks https://github.com/ScholzVolkmer/django-twoscoops-project.git
+cd django-twoscoops-project
+git submodule update --init --recursive
+git submodule foreach git pull origin master
+cd ..
+django-admin.py startproject --template=django-twoscoops-project -n Vagrantfile,default.rb -e html $1
+cd $1
+rm .gitmodules
+find . -name ".git" -delete
+git init
+git add .
+git commit -m "Initial commit"
+vagrant up
+vagrant provision
+rm -rf django-twoscoops-project
