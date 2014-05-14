d4986eef8d06017d43db907088bfed21f851fb6c
Sergey Cheparev <s.cheparev@s-v.de>
2014-05-14 12:37:37 +0200

    Removed provisioning from start project script

--- utils/start_project.sh
+++ utils/start_project.sh
@@ -2,4 +2,3 @@
 django-admin.py startproject --template=https://github.com/ScholzVolkmer/django-twoscoops-project/archive/develop.zip -n Vagrantfile,Berksfile,metadata.rb,default.rb -e html $1
 cd $1
 vagrant up
-vagrant provision
