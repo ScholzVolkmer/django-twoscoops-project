16d70316318703ff36b2c73a1105406d9b313c2c
Sergey Cheparev <s.cheparev@s-v.de>
2014-06-05 15:17:52 +0200

    Moved to utils and renamed chef installation script

--- utils/install_chef_kit.sh
+++ utils/install_chef_kit.sh
@@ -5,8 +5,6 @@ chef_binary=/usr/local/bin/chef-solo
 
 if ! test -f "$chef_binary"; then
 	export DEBIAN_FRONTEND=noninteractive
-	# Upgrade headlessly (this is only safe-ish on vanilla systems)
-	apt-get dist-upgrade
 	apt-get update
 	apt-get install -y ruby1.9.1 ruby1.9.1-dev make g++ autoconf git
 	wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.1.0-1_amd64.deb
