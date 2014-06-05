967142755a47da5b9ee602661e298981d2abef2a
Sergey Cheparev <s.cheparev@s-v.de>
2014-06-05 17:04:50 +0200

    Added user and environment taken from json config

--- chef/cookbooks/project_name/recipes/server.rb
+++ chef/cookbooks/project_name/recipes/server.rb
@@ -3,11 +3,13 @@ include_recipe "{{project_name}}::default"
 include_recipe "apache2"
 
 root = File.absolute_path(File.dirname(__FILE__)) + "/../../../../"
+main_user = node['{{project_name}}']['user']
+environment = node['{{project_name}}']['environment']
 
 python_virtualenv root + "virtualenv" do
   Chef::Log.info("Virtualenv...")
-  owner "jenkins"
-  group "jenkins"
+  owner "#{main_user}"
+  group "#{main_user}"
   action :create
 end
 
@@ -15,17 +17,17 @@ python_pip root + "requirements.txt" do
   Chef::Log.info("Create virtualenv")
   virtualenv root + "virtualenv"
   action :install_requirements
-  user "jenkins"
-  group "jenkins"
+  user "#{main_user}"
+  group "#{main_user}"
 end
 
-ENV['APP_SETTINGS'] = "production"
+ENV['APP_SETTINGS'] = "#{environment}"
 venv_python = root + "virtualenv/bin/python"
 manage_py = root + "{{project_name}}/manage.py"
 execute "#{venv_python} #{manage_py} syncdb --noinput --migrate" do
   Chef::Log.info("Migration/syncdb")
-  user "jenkins"
-  group "jenkins"
+  user "#{main_user}"
+  group "#{main_user}"
 end
 
 web_app "{{project_name}}" do
@@ -33,15 +35,15 @@ web_app "{{project_name}}" do
   docroot root
   cookbook "{{project_name}}"
   server_admin "admin@example.com"
-  wsgi_script_alias "#{root}{{project_name}}/{{project_name}}/wsgi/production.py"
+  wsgi_script_alias "#{root}{{project_name}}/{{project_name}}/wsgi/#{environment}.py"
   static_alias "#{root}{{project_name}}/static"
   media_alias "#{root}{{project_name}}/media"
-  user 'jenkins'
+  user '#{main_user}'
   log_dir "#{root}logs"
 end
 
 execute "#{venv_python} #{manage_py} collectstatic --noinput" do
   Chef::Log.info("Collectstatic")
-  user "jenkins"
-  group "jenkins"
+  user "#{main_user}"
+  group "#{main_user}"
 end
