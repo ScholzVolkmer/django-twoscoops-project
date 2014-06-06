3b7fba7ed19a8eb0c17f1b9af08885b13466b3db
Sergey Cheparev <s.cheparev@s-v.de>
2014-06-06 10:18:07 +0200

    Removed default vagrant up for start project utility

--- utils/start_project.sh
+++ utils/start_project.sh
@@ -2,4 +2,4 @@
 django-admin.py startproject --template=https://github.com/ScholzVolkmer/vachango/archive/develop.zip\
  -n Vagrantfile,Makefile,Vagrantfile_servertest,Berksfile,.gitignore -e html,erb,rst,json,bat,rb,py $1
 cd $1
-vagrant up
+
